fbd73aba39c29e29ff9f1c166f1e0ac5c93327dc
camilomupu <danielcamilomu35@gmail.com>
2024-10-08 14:04:24 -0500

    Correcion

--- config/data_notion.py
+++ config/data_notion.py
@@ -1,11 +1,11 @@
 from notion_client import Client
 import os
 
-# Cargar la clave de Notion desde las variables de entorno o desde un archivo de configuración seguro.
-NOTION_TOKEN = os.getenv('NOTION_TOKEN')
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 
 # Inicializa el cliente de Notion
 notion = Client(auth=NOTION_TOKEN)
+
 
 def list_databases():
     """
@@ -15,45 +15,46 @@ def list_databases():
     databases_list = []
     try:
         databases = notion.search(filter={"property": "object", "value": "database"})
-        for result in databases['results']:
-            title = result['title'][0]['plain_text']
-            databases_list.append((title, result['id']))
-            #print(f"ID: {result['id']}, Title: {title}")
+        for result in databases["results"]:
+            title = result["title"][0]["plain_text"]
+            databases_list.append((title, result["id"]))
+            # print(f"ID: {result['id']}, Title: {title}")
     except Exception as e:
         print(f"Error buscando databases: {e}")
     return databases_list
+
 
 def get_notion_data():
     """
     Función para obtener los campos ID y Link de todas las bases de datos de Notion.
     """
     data = []
-    databases = list_databases()  # Obtener la lista de bases de datos
+    databases = list_databases()
 
     for title, database_id in databases:
         try:
-            # Realiza la consulta a cada base de datos
             response = notion.databases.query(database_id=database_id)
 
             # Extraer los resultados
-            for item in response.get('results', []):
-                # Captura solo los campos ID y Link
-                row = {
-                    'ID': None,  # Inicializa como None
-                    'Link': None  # Inicializa como None
-                }
+            for item in response.get("results", []):
+                row = {"ID": None, "Link": None}
 
                 # Verificar y obtener el ID
                 try:
-                    if item['properties']['ID']['rich_text']:
-                        row['ID'] = item['properties']['ID']['rich_text'][0]['text']['content']
+                    if item["properties"]["ID"]["rich_text"]:
+                        row["ID"] = item["properties"]["ID"]["rich_text"][0]["text"][
+                            "content"
+                        ]
                 except (IndexError, KeyError):
                     print(f"ID no disponible para el elemento: {item}")
 
                 # Verificar y obtener el Link
                 try:
-                    if 'Link' in item['properties'] and item['properties']['Link']['url']:
-                        row['Link'] = item['properties']['Link']['url']
+                    if (
+                        "Link" in item["properties"]
+                        and item["properties"]["Link"]["url"]
+                    ):
+                        row["Link"] = item["properties"]["Link"]["url"]
                 except KeyError:
                     print(f"Link no disponible para el elemento: {item}")
 
